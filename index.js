008dd682ff3d98ff0353573fe1eaed40f8fd5dfd
kannapatton <kandirbrts@gmail.com>
2022-10-10 20:30:18 -0500

    cors

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(function(req, res, next) {
-	res.header('Acess-Control-Allow-Methods', 'POST, GET, DELETE');
-	res.header('Access-Control-Allow-Origin', 'https://mpfront.vercel.app'); // update to match the domain you will make the request from
-	res.header('Access-Control-Allow-Credentials', true);
-	res.header('Content-Type', 'application/json');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', '*');
-	next();
-});
+// app.use(function(req, res, next) {
+// 	res.header('Acess-Control-Allow-Methods', 'POST, GET, DELETE');
+// 	res.header('Access-Control-Allow-Origin', 'https://mpfront.vercel.app'); // update to match the domain you will make the request from
+// 	res.header('Access-Control-Allow-Credentials', true);
+// 	res.header('Content-Type', 'application/json');
+// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// 	res.header('Access-Control-Allow-Methods', '*');
+// 	next();
+// });
 
 const { logger } = require('./middleware/index.js');
 const authRouter = require('./routes/auth');
